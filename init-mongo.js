9dda5cfbe1d8d65658a27c3bd7e55c7f18f0f300
richjjoh <71095198+richjjoh@users.noreply.github.com>
2024-01-09 16:31:31 +0000

    Create init-mongo.js
    Created init-mongo.js.

---
+++ init-mongo.js
@@ -0,0 +1,4 @@
+# unifipwd should be replaced with password, and set inside docker-compose.yml . 
+
+db.getSiblingDB("unifi").createUser({user: "unifi", pwd: "unifipwd", roles: [{role: "readWrite", db: "unifi"}]});
+db.getSiblingDB("unifi_stat").createUser({user: "unifi", pwd: "unifipwd", roles: [{role: "readWrite", db: "unifi_stat"}]});
